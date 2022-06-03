0a44b002bdd9f8b19867afcb2c13d83839f75f3a
Carson <carsongro@gmail.com>
2022-06-03 15:38:26 -0600

    Initial Commit

---
+++ edutainment/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  edutainment
+//
+//  Created by Carson Gross on 6/3/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ edutainment/edutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  edutainmentApp.swift
+//  edutainment
+//
+//  Created by Carson Gross on 6/3/22.
+//
+
+import SwiftUI
+
+@main
+struct edutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
